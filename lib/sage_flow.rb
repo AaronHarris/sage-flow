b141f1154eb23a3c6ad24c6a6749371bc99d0279
Francis Potter <git@fpotter.com>
2012-06-13 13:10:14 -0700

    Copyright notice

--- lib/sage_flow.rb
+++ lib/sage_flow.rb
@@ -1,3 +1,14 @@
+=begin
+  SageFlow gem
+  Copyright (c) 2012 The Hathersage Group, Inc.
+  All Rights Reserved
+  The Hathersage Group, Inc.
+  9415 Culver Blvd
+  Culver City, CA 90232
+  http://www.hathersagegroup.com
+=end
+
+
 module SageFlow
   def self.included(base)
     base.extend(ClassMethods)
