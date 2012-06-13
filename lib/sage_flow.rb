cd93e20ee9c99ecc2f53067b81f8f3f14e2ea059
Francis Potter <git@fpotter.com>
2012-06-13 12:18:13 -0700

    Include and extend in SageFlow

--- lib/sage_flow.rb
+++ lib/sage_flow.rb
@@ -1,2 +1,15 @@
 module SageFlow
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+  
+  module ClassMethods
+    def class_method
+      'Hello from Class Method'
+    end
+  end
+  
+  def test_method
+    "Hello World"
+  end
 end
--- spec/flow_spec.rb
+++ spec/flow_spec.rb
@@ -4,4 +4,17 @@ describe SageFlow do
   it "does something" do
     true.should be_true
   end
+  it "can be included" do
+    class Foo
+      include SageFlow
+    end
+    f = Foo.new
+    f.test_method.should == "Hello World"
+  end
+  it "adds class methods" do
+    class Foo
+      include SageFlow
+    end
+    Foo.class_method.should == "Hello from Class Method"
+  end
 end
